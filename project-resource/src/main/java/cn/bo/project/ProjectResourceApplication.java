11fef2b61ba36db3f3d2b17f148af7cb047fd1b3
zhangbo <773463159@qq.com>
2020-07-28 23:37:20 +0800

    oss初始化

---
+++ project-resource/src/main/java/cn/bo/project/ProjectResourceApplication.java
@@ -0,0 +1,13 @@
+package cn.bo.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectResourceApplication.class, args);
+    }
+
+}
